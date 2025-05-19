12c5b5fec06fa434cdb6c4dfa613722550e0e143
Átila Alves Rodrigues <atila.a@dinfor.uniceub>
2025-05-19 20:22:11 -0300

    Services e Rpositorys

---
+++ MBGestaoEscolarAN/Repository/Interfaces/ITurmaRepository.cs
@@ -0,0 +1,13 @@
+﻿using MBGestaoEscolarAN.Entities;
+
+namespace MBGestaoEscolarAN.Repository.Interfaces
+{
+    public interface ITurmaRepository
+    {
+        Task<IEnumerable<Turma>> ListarTodosAsync();
+        Task<Turma> ListarPorIdAsync(int id);
+        Task AdicionarAsync(Turma turma);
+        Task AlterarAsync(Turma turma);
+        Task ExcluirAsync(int id);
+    }
+}
